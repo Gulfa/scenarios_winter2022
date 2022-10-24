ce0feff2c7bbca9e01ca2d7d5f1ea97cf3311a41
gulfa <gunnarroe@gmail.com>
2022-10-24 21:53:21 +0200

    Initial commit with working scenarios for same variant

---
+++ README.md
@@ -0,0 +1,4 @@
+# Covid-19 winter scenarios for Norway
+
+To run the scenarios edit the number of threads in the mclapply function call in run_analysis.R and run the file. This creates a all_results.RDS file that can then be plotted using plot_results.R
+
---
+++ install_reqs.R
@@ -0,0 +1,17 @@
+
+
+drat:::add("ncov-ic")
+install.packages(c("abind", "tideyverse", "data.table", "odin", "odin.dust", "dust", "mcstate", "doBy", "lhs", "truncnorm"))
+
+options(
+  repos = structure(c(
+    SPLVERSE  = "https://docs.sykdomspulsen.no/drat/",
+    CRAN      = "https://cran.rstudio.com"
+  ))
+)
+
+install.packages("spldata")
+
+
+devtools::install_github(repo="https://github.com/Gulfa/metapop")
+devtools::install_github(repo="https://github.com/Gulfa/metapopnorge")
